7b724b6ce62a7ef099c0b3003d83780569b431c1
j1000tonm <j1000tonm.jmb@gmail.com>
2020-09-16 00:04:27 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.dspesquisa.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.devsuperior.dspesquisa.services.GameService;
 @RequestMapping(value = "/games")
 public class GameResource {
 	
+	@Autowired
 	private GameService service;
 	
 	@GetMapping
